1ff2fe402d1244333000bdca5b1561eb32b0fa0c
Collin Miller <collintmiller@gmail.com>
2023-07-21 11:23:54 -0500

    Logs all queries in execQuery

--- src/db/execQuery.ts
+++ src/db/execQuery.ts
@@ -1,5 +1,6 @@
 import { pool } from "./pool";
 
 export const execQuery = (query: string, values?: []) => {
+  console.log("Executing query:", { query, values });
   return pool.query(query, values);
 };
