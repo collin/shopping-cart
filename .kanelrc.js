082810094a1cb40bd0387e4c65c6520215dd7c4a
Collin Miller <collintmiller@gmail.com>
2023-07-21 14:45:55 -0500

    Generates typescript types from the database schemea with kanel

---
+++ .kanelrc.js
@@ -0,0 +1,10 @@
+require("dotenv/config");
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing env var: DATABASE_URL");
+}
+
+module.exports = {
+  outputPath: "./src/types",
+  connection: process.env.DATABASE_URL,
+};
