5af6c8034e3b28c4614d6350b07e5f8bc7444ed6
Collin Miller <collintmiller@gmail.com>
2023-07-21 13:02:31 -0500

    Install basic front-end routing

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,7 +1,19 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/catalog">Catalog</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home</div>} />
+        <Route path="/catalog" element={<div>Catalog</div>} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
