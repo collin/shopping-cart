64ddf93dd21fd2b7de422746ad540a93290d17bf
Collin Miller <collintmiller@gmail.com>
2023-07-21 11:26:04 -0500

    Add script to drop and re-create database schema for products

---
+++ src/db/setupDB.ts
@@ -0,0 +1,29 @@
+import { execQuery } from "./execQuery";
+
+async function setupDB() {
+  // drop existing tables/schema
+  await execQuery(/* SQL */ `
+    DROP SCHEMA IF EXISTS store CASCADE;
+  `);
+
+  // create new tables/schema
+  await execQuery(/* SQL */ `CREATE SCHEMA store`);
+  // Table store.products {
+  //   id bigserial pk // integer
+  //   title text [not null]
+  //   description text [not null]
+  //   price integer [not null, default: 0, note: 'must be >= 0']
+  // }
+
+  await execQuery(/* SQL */ `
+    CREATE TABLE store.products (
+      id BIGSERIAL PRIMARY KEY, 
+      title TEXT NOT NULL,
+      description TEXT NOT NULL,
+      price INTEGER NOT NULL CHECK (price >= 0)
+    )`);
+
+  // create initial data
+}
+
+setupDB();
