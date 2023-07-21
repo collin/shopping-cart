7b4f61ea9b3a27b9d494de7f2aa8976d4287615e
Collin Miller <collintmiller@gmail.com>
2023-07-21 11:24:58 -0500

    Requires DATABASE_URL in order to boot system

--- src/db/pool.ts
+++ src/db/pool.ts
@@ -1,5 +1,9 @@
 import { Pool } from "pg";
 
+if (!process.env.DATABASE_URL) {
+  throw new Error("Missing env var: DATABASE_URL");
+}
+
 export const pool = new Pool({
   // postgres://postgres:postgres@localhost:5432/shopping-cart
   connectionString: process.env.DATABASE_URL,
