f54c486670e767b034728de0ae805780a59dd0ed
Collin Miller <collintmiller@gmail.com>
2023-07-21 12:32:25 -0500

    Installed vite-express to build client app

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import express from "express";
+import ViteExpress from "vite-express";
 // @ts-expect-error - no types available
 import volleyball from "volleyball";
 
@@ -8,10 +9,10 @@ const port = parseInt(process.env.PORT || "3000");
 
 app.use(volleyball);
 
-app.get("/", (req, res) => {
+app.get("/test", (req, res) => {
   res.send("The sedulous hyena ate the antelope!");
 });
 
-app.listen(port, () => {
+ViteExpress.listen(app, port, () => {
   return console.log(`server is listening on ${port}`);
 });
