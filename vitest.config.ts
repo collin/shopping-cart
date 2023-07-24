326d8d7ba7b93002cf32b348d735b616aa740a9e
Collin Miller <collintmiller@gmail.com>
2023-07-24 09:34:01 -0500

    Fix test config location of global setup file

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    globalSetup: "./globalTestSetup.ts",
+    globalSetup: "./src/globalTestSetup.ts",
   },
 });
