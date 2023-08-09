af8b4ac7f2d82e2b26a1383c2e68fd43fec1968d
Collin Miller <collintmiller@gmail.com>
2023-08-09 12:28:49 -0500

    Adds a hard-coded user for ease of manual testing

--- src/db/seedDB.ts
+++ src/db/seedDB.ts
@@ -1,7 +1,11 @@
 import { seedProducts } from "./seeds/seedProducts";
-import { seedUsers } from "./seeds/seedUsers";
+import { seedUser, seedUsers } from "./seeds/seedUsers";
 
 export async function seedDB() {
+  // These seeds are randomized
   await seedProducts();
   await seedUsers();
+
+  // These seeds are not-randomized
+  await seedUser("Test User", "test-user@example.org", "123456");
 }
